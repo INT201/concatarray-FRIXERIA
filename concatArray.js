72143e4f9d2c80256b63d9eeddea698ac2c4ac1b
FRIXERIA <pattharaphon.saef@kmutt.ac.th>
2022-09-10 13:42:54 +0700

    Upadte#2

--- concatArray.js
+++ concatArray.js
@@ -1,5 +1,4 @@
 const { template } = require('@babel/core')
-
 function concatArray(array1, array2) {
 if (array1===null || array1===undefined && array2===null||array2===undefined) {
   return undefined
@@ -11,6 +10,5 @@ if (array1===null || array1===undefined && array2===null||array2===undefined) {
   return array1
 }else
   return array1.concat(array2)
-
 }
 module.exports = concatArray
